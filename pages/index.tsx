20f853f0c4562663b4f61f4501d3f51439fd7672
joaov <jpvf482@gmail.com>
2022-05-15 23:38:29 -0300

    incluidas opções de variação para alguns parametros

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
     Area,
     AreaChart,
@@ -13,18 +14,30 @@ type ChartPointType = {
     T: number;
 };
 
+type OptionType = { label: string; value: number };
+
 export default function Home() {
     //Ar
     const T_inf = 25; // °C
     const h = 100; // W/(m^2*K)
 
-    //Cobre
-    const k = 398; // W/(m*K)
+    //k
+    const k_copper = 398; // W/(m*K)
+    const k_steel = 14;
+    const k_aluminum = 180;
+
+    const options: OptionType[] = [
+        { label: "copper", value: k_copper },
+        { label: "steel", value: k_steel },
+        { label: "aluminum", value: k_aluminum },
+    ];
 
     //Geometria
     const d = 0.005; // m (5 mm)
     const T_b = 100; // °C
-    const x = 0.1;
+    const [x, setX] = useState(0.05);
+
+    const [k, setK] = useState(options[0].value);
 
     function temperatureDistribution(m: number, x: number): number {
         return Math.exp(-m * x);
@@ -91,7 +104,17 @@ export default function Home() {
         <div>
             <h2>PARTE INTERNA APENAS</h2>
             <h3>Resultados:</h3>
-            <h4>Para x = 50 mm:</h4>
+            <h4>
+                Para x ={" "}
+                <input
+                className="p-1 w-16"
+                    type="number"
+                    step={1}
+                    value={Math.round(x * 100)}
+                    onChange={(event) => setX(Number(event.target.value) / 100)}
+                />{" "}
+                mm:
+            </h4>
             <p>
                 theta/theta_b ={" "}
                 {temperatureDistribution(m(h, P(d), k, A_tr(d / 2)), x)}
@@ -110,39 +133,52 @@ export default function Home() {
                     temperatureDistribution(m(h, P(d), k, A_tr(d / 2)), x)
                 )}
             </p>
-            <div className="bg-white m-4 w-[600px] h-[300px]">
-                <ResponsiveContainer width="100%" height="100%">
-                    <AreaChart
-                        width={600}
-                        height={300}
-                        data={generateChartPoints(
-                            0,
-                            0.4,
-                            0.025,
-                            (x_value: number) => {
-                                return T(
-                                    T_inf,
-                                    T_b,
-                                    temperatureDistribution(
-                                        m(h, P(d), k, A_tr(d / 2)),
-                                        x_value
-                                    )
-                                );
-                            }
-                        )}
-                    >
-                        <CartesianGrid stroke="#ccc" />
-                        <XAxis dataKey="x" />
-                        <YAxis type="number" domain={[20, 100]} />
-                        <Tooltip />
-                        <Area
-                            type="monotone"
-                            dataKey="T"
-                            stroke="#8884d8"
-                            fill="#8884d8"
-                        />
-                    </AreaChart>
-                </ResponsiveContainer>
+            Selecione o material:{" "}
+            <select
+                value={k}
+                onChange={(event) => setK(Number(event.target.value))}
+            >
+                {options.map(({ label, value }, index) => (
+                    <option value={value} key={index}>
+                        {label}
+                    </option>
+                ))}
+            </select>
+            <div className="bg-white p-4">
+                <div className="w-[600px] h-[300px]">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart
+                            width={600}
+                            height={300}
+                            data={generateChartPoints(
+                                0,
+                                0.4,
+                                0.025,
+                                (x_value: number) => {
+                                    return T(
+                                        T_inf,
+                                        T_b,
+                                        temperatureDistribution(
+                                            m(h, P(d), k, A_tr(d / 2)),
+                                            x_value
+                                        )
+                                    );
+                                }
+                            )}
+                        >
+                            <CartesianGrid stroke="#ccc" />
+                            <XAxis dataKey="x" />
+                            <YAxis type="number" domain={[20, 100]} />
+                            <Tooltip />
+                            <Area
+                                type="monotone"
+                                dataKey="T"
+                                stroke="#8884d8"
+                                fill="#8884d8"
+                            />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     );
